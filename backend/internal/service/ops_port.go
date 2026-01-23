db134539278204f27ae8f55b47e2d7452d34fddb
song <slovx2@gmail.com>
2026-01-23 22:29:06 +0800

    Fix gofmt

--- backend/internal/service/ops_port.go
+++ backend/internal/service/ops_port.go
@@ -176,7 +176,7 @@ type OpsInsertSystemMetricsInput struct {
 	Upstream429Count             int64
 	Upstream529Count             int64
 
-	TokenConsumed int64
+	TokenConsumed      int64
 	AccountSwitchCount int64
 
 	QPS *float64
@@ -239,8 +239,8 @@ type OpsSystemMetricsSnapshot struct {
 	DBConnIdle    *int `json:"db_conn_idle"`
 	DBConnWaiting *int `json:"db_conn_waiting"`
 
-	GoroutineCount        *int `json:"goroutine_count"`
-	ConcurrencyQueueDepth *int `json:"concurrency_queue_depth"`
+	GoroutineCount        *int   `json:"goroutine_count"`
+	ConcurrencyQueueDepth *int   `json:"concurrency_queue_depth"`
 	AccountSwitchCount    *int64 `json:"account_switch_count"`
 }
 
